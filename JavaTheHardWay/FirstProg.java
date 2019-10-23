715ba2c8fe25332a847045ed67077ebbdc45c50a
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-23 13:33:09 -0500

    Actually finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main(String[] args) {
+    System.out.println("I, Marcus Wiseman, am determined to learn how to code.");
+    System.out.println("Today's date is Wednesday, October 23, 2019.");
+  }
+}
