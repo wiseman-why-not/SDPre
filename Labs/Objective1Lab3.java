b7b83ad23d2e548c43342ea13cc1d2b416e0ad06
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-23 14:40:32 -0500

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[] args) {
+    System.out.println("Name : Marcus Wiseman");
+    System.out.println("Age : 26");
+    System.out.println("State : Oklahoma");
+  }
+}
