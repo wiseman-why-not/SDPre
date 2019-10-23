872677c54ce81421742e153095e2a331e1d088cf
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-23 14:06:34 -0500

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt {
+  public static void main(String[] args) {
+    System.out.println("+-----------------------+");
+    System.out.println("|                       |");
+    System.out.println("|      CORNER STORE     |");
+    System.out.println("|                       |");
+    System.out.println("| 2019-10-23  1:53PM    |");
+    System.out.println("|                       |");
+    System.out.println("|                       |");
+    System.out.println("| Gallons:       13.999 |");
+    System.out.println("| Price/gallon: $ 2.491 |");
+    System.out.println("|                       |");
+    System.out.println("| Fuel total:   $ 34.87 |");
+    System.out.println("|                       |");
+    System.out.println("+-----------------------+");
+
+  }
+}
