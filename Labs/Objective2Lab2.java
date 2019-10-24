f410cb822aed4205de8eb4876e06a86e5b2cb423
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-23 19:32:28 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+  public static void main(String[] args) {
+    String name = "Marcus Wiseman";
+    int age = 26;
+    String state = "Oklahoma";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
