2fdd55f76535510b8ad3826269ec13aee04afbf3
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-23 23:45:25 -0500

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3{
+  public static void main(String[] args) {
+    String food1, food2, food3;
+    food1 = "Chicken and Waffles";
+    food2 = "Smoked Salmon";
+    food3 = "pizza";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
