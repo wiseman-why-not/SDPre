633eea6f9629cf45079b211f32f823a6fe7f54c0
wiseman-why-not <marcus.e.wiseman@gmail.com>
2019-10-27 19:08:58 -0500

    complete Objective7lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+      /*
+      Your solution goes here
+      */
+      printMenu();
+      selection = scanner.nextInt();
+
+      if (selection == 1) {
+        System.out.println("Hello Human");
+      }
+      else if (selection == 2) {
+        System.out.println("Apples, Bananas, Coconuts");
+      }
+      else if (selection == 3) {
+        System.out.println("Goodbye");
+        break;
+      }
+      else if (selection != 1 || selection != 2 || selection != 3 ) {
+        System.out.println("Selected wrong number!");
+      }
+
+    }
+
+    scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
